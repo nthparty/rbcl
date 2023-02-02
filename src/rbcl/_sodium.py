95aea720c76f4ec17e61fc0cb57153518bd71e5b
bengetch <bengetch@gmail.com>
2023-02-02 17:08:50 -0500

    .pyd

--- src/rbcl/_sodium.py
+++ src/rbcl/_sodium.py
@@ -12,13 +12,13 @@ pl = platform.system()
 
 # Read sodium shared object file from disk
 sodium_so = open(  # pylint: disable=consider-using-with
-    "src/rbcl/sodium.dll" if pl == "Windows" else "src/rbcl/sodium.so", "rb"
+    "src/rbcl/sodium.pyd" if pl == "Windows" else "src/rbcl/sodium.so", "rb"
 )
 bs = sodium_so.read()
 sodium_so.close()
 
 # Generate a temporary file on disk and write sodium binary to it
-LIB_EXT = ".dll" if pl == "Windows" else ".so"
+LIB_EXT = ".pyd" if pl == "Windows" else ".so"
 lib_path = tempfile.NamedTemporaryFile(suffix=LIB_EXT).name  # pylint: disable=consider-using-with
 sodium_tmp = open(lib_path, "wb")  # pylint: disable=consider-using-with
 sodium_tmp.write(bs)
