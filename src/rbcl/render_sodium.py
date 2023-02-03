2a35f87b7878ce0f10cbb74eb3442dabad65541b
bengetch <bengetch@gmail.com>
2023-02-03 14:04:02 -0500

    Add extension for windows

--- src/rbcl/render_sodium.py
+++ src/rbcl/render_sodium.py
@@ -1,10 +1,15 @@
 """
 Dynamically render libsodium from .so/.pyd file during GH Actions build
 """
+import platform
 import pystache
 
+# Determine OS type
+pl = platform.system()
+LIB_EXT = ".pyd" if pl == "Windows" else ".so"
+
 data = {
-    "SODIUM_HEX": open("src/rbcl/sodium.so", "rb").read().hex()  # pylint: disable=consider-using-with
+    "SODIUM_HEX": open(f"src/rbcl/sodium.{LIB_EXT}", "rb").read().hex()  # pylint: disable=consider-using-with
 }
 template = open("src/rbcl/_sodium.tmpl", encoding='utf-8').read()  # pylint: disable=consider-using-with
 
