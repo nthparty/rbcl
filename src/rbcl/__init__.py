eb2f6ae69fbaabc5a643436b64c3ee966826823e
Andrei Lapets <a@lapets.io>
2023-04-23 11:06:52 -0400

    Fix incorrect import/qualification for libsodium initialization call.

--- src/rbcl/__init__.py
+++ src/rbcl/__init__.py
@@ -1,5 +1,5 @@
 """Gives users direct access to functions."""
-from rbcl.rbcl import _sodium # pylint: disable=import-self
+from rbcl.rbcl import _sodium, _sodium_init # pylint: disable=import-self
 
 from rbcl.rbcl import \
     crypto_scalarmult_ristretto255, \
@@ -29,4 +29,4 @@ from rbcl.rbcl import \
     randombytes_buf_deterministic
 
 # Check that libsodium is not already initialized and initialize it.
-_sodium._sodium_init() # pylint: disable=protected-access
+_sodium_init() # pylint: disable=protected-access
